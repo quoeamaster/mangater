652ab9b2f7fae7d81ce7313e1034735919ef35cc
takara-mono <quoeamaster@gmail.com>
2026-02-15 19:39:43 +0800

    2026-02 - add unit for util::load_from_env()

--- crates/mangater-core/src/util/config.rs
+++ crates/mangater-core/src/util/config.rs
@@ -67,3 +67,51 @@ pub fn load_from_json(json_file: Option<String>) -> Result<Option<String>, SdkEr
     // as loading a json file, the contents are supposed to be returned (contrast to load_from_env() which returns nothing)
     Ok(Some(config_in_json))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_from_env() {
+        // .env file does not exist; hence will have error for sure
+        let result = load_from_env(None);
+        assert!(result.is_err());
+        assert!(
+            result
+                .err()
+                .unwrap()
+                .to_string()
+                .contains("Failed to load environment variables, make sure the env file")
+        );
+        // again but checking error type instead
+        let result = load_from_env(None);
+        match result {
+            Ok(_) => panic!("Expected error (default .env file does not exist), got Ok"),
+            // make sure the error type is SdkError::NotFound
+            Err(SdkError::NotFound(msg)) => assert!(
+                msg.contains("Failed to load environment variables, make sure the env file")
+            ),
+            Err(e) => panic!("Expected SdkError::NotFound, got {:?}", e),
+        }
+        // again with a valid custom .env file
+        let result = load_from_env(Some("testing/.env-unit-test".to_string()));
+        assert!(result.is_ok());
+        match std::env::var("unit.test.sequence") {
+            Ok(value) => assert_eq!(value, "9801"),
+            Err(e) => panic!("Expected Ok, got {:?}", e),
+        }
+        match std::env::var("unit.test.message") {
+            Ok(value) => assert_eq!(value, "happy plugin dev"),
+            Err(e) => panic!("Expected Ok, got {:?}", e),
+        }
+        match std::env::var("non-exist-var") {
+            Ok(_) => panic!("Expected Err, got Ok"),
+            Err(e) => {
+                // [debug]
+                // println!("error: {:?}", e.to_string());
+                assert!(e.to_string().contains("environment variable not found"))
+            }
+        }
+    }
+}
